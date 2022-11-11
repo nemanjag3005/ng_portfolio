726c83f64c6307eafba455672d91bb27ec1a18c5
Nemanja Grujic <luxisoop@gmail.com>
2022-11-11 16:09:07 -0700

    fix

--- components/PortfolioPage.js
+++ components/PortfolioPage.js
@@ -336,9 +336,9 @@ const PortfolioPage = () => {
                         className="h-5 w-5"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                         ></path>
                       </svg>
@@ -365,9 +365,9 @@ const PortfolioPage = () => {
                         className="h-5 w-5"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                         ></path>
                       </svg>
@@ -397,9 +397,9 @@ const PortfolioPage = () => {
                         className="h-5 w-5"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
                         ></path>
                       </svg>
