0ba2ace00e8fa4b08de159821471370b4b0fe105
nemanjag3005 <nemanjag3005@gmail.com>
2025-06-12 11:32:19 +0100

    update hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -49,14 +49,10 @@ const Hero = ({
                   </div>
                   <p className="text-xl md:text-2xl md:pr-10">
                     {/* {dictionary.desc} */}
-                    I’m a graduate student at Imperial College London,
-                    specializing in Bioinformatics, where I apply my
-                    computational skills to solve biological problems. In my
-                    free time, I’m a{" "}
-                    <span className="text-dark font-semibold">
-                      full-stack developer
-                    </span>{" "}
-                    and aspiring entrepreneur building several digital projects.
+                    I’m a graduate student at Imperial College London working at
+                    the intersection of biology and computation. On the side, I
+                    build full-stack software projects — systems that are clean,
+                    performant, and human-centric.
                   </p>
                   <div className="grid grid-cols-1 gap-2 space-y-1 lg:space-y-0 md:flex md:space-x-3 items-center w-full">
                     <div className="flex items-center space-x-2 w-full">
